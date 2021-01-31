87c4f27103429e4f194610d07cba7bf0bc1f845f
truongaxin123 <truongaxin123@gmail.com>
2021-01-31 19:45:50 +0700

    add helper

---
+++ cs4rsa/Helper/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa.Helper
+{
+    /// <summary>
+    /// Class này chứa những phương thức hữu ích.
+    /// </summary>
+    class Helper
+    {
+        public static string getTimeFromEpoch()
+        {
+            long fromUnixEpoch = (long)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalMilliseconds;
+            return fromUnixEpoch.ToString();
+        }
+    }
+}
