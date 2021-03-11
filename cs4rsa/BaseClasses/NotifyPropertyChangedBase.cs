df2d2a1fdee72833dd967331638f6e41992782f5
truongaxin123 <truongaxin123@gmail.com>
2021-03-11 23:01:39 +0700

    add baseclass

---
+++ cs4rsa/BaseClasses/NotifyPropertyChangedBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace cs4rsa.BaseClasses
+{
+    public class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged([CallerMemberName] string property = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
