c884f5e255ebc2dc0776b8e4908e5cafa721017b
truongaxin123 <truongaxin123@gmail.com>
2021-03-11 23:02:45 +0700

    add class group view model

---
+++ cs4rsa/ViewModels/ClassGroupViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using cs4rsa.BasicData;
+using cs4rsa.Models;
+using cs4rsa.BaseClasses;
+using cs4rsa.ViewModels;
+using System.Runtime.CompilerServices;
+
+namespace cs4rsa.ViewModels
+{
+    public class ClassGroupViewModel : NotifyPropertyChangedBase
+    {
+        public static EventHandler SelectedSubjectChanged;
+
+        private ObservableCollection<ClassGroupModel> classGroupModels = new ObservableCollection<ClassGroupModel>();
+        public ObservableCollection<ClassGroupModel> ClassGroupModels
+        {
+            get
+            {
+                return classGroupModels;
+            }
+            set
+            {
+                classGroupModels = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private ClassGroupModel selectedClassGroup;
+        public ClassGroupModel SelectedClassGroup
+        {
+            get
+            {
+                return selectedClassGroup;
+            }
+            set
+            {
+                selectedClassGroup = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public ClassGroupViewModel()
+        {
+            SelectedSubjectChanged += OnSelectedSubjectChanged;
+        }
+
+        private void OnSelectedSubjectChanged(object sender, EventArgs e)
+        {
+            SubjectModel subjectModel = (SubjectModel)sender;
+            classGroupModels.Clear();
+            foreach(ClassGroupModel classGroupModel in subjectModel.ClassGroupModels)
+            {
+                classGroupModels.Add(classGroupModel);
+            }
+        }
+    }
+}
