aeaea03620300cf2cab8d132a2f0ea4db52ae29b
truongaxin123 <truongaxin123@gmail.com>
2021-04-24 15:22:15 +0700

    rename message

---
+++ cs4rsa/BaseClasses/Cs4rsaMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LightMessageBus.Interfaces;
+
+namespace cs4rsa.BaseClasses
+{
+    public class Cs4rsaMessage : IMessage
+    {
+        public object Source { get; set; }
+
+        public Cs4rsaMessage(object source)
+        {
+            Source = source;
+        }
+    }
+}
