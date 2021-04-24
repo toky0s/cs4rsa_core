da1cb330f1cf4e4d1fb6b8f3f2d1af944124caef
truongaxin123 <truongaxin123@gmail.com>
2021-04-24 15:40:27 +0700

    add message

---
+++ cs4rsa/Messages/DeleteSubjectMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cs4rsa.BaseClasses;
+using cs4rsa.Models;
+
+namespace cs4rsa.Messages
+{
+    class DeleteSubjectMessage : Cs4rsaMessage
+    {
+        public readonly new SubjectModel Source;
+        public DeleteSubjectMessage(SubjectModel source) : base(source)
+        {
+            Source = source;
+        }
+    }
+}
