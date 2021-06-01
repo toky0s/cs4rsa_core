43a44ac32619e6adb43dd8fd1a98ec3c168d187a
truongaxin123 <truongaxin123@gmail.com>
2021-06-01 16:38:17 +0700

    add messagebox service

---
+++ cs4rsa/Dialogs/MessageBoxService/Cs4rsaMessageBox.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+
+namespace cs4rsa.Dialogs.MessageBoxService
+{
+    class Cs4rsaMessageBox : IMessageBox
+    {
+        public MessageBoxResult ShowMessage(string message)
+        {
+            return MessageBox.Show(message);
+        }
+
+        public MessageBoxResult ShowMessage(string message, string caption)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MessageBoxResult ShowMessage(string message, string caption, MessageBoxButton button)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MessageBoxResult ShowMessage(string message, string caption, MessageBoxButton button, MessageBoxImage icon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ cs4rsa/Dialogs/MessageBoxService/IMessageBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace cs4rsa.Dialogs.MessageBoxService
+{
+    public interface IMessageBox
+    {
+        MessageBoxResult ShowMessage(string message);
+        MessageBoxResult ShowMessage(string message, string caption);
+        MessageBoxResult ShowMessage(string message, string caption, MessageBoxButton button);
+        MessageBoxResult ShowMessage(string message, string caption, MessageBoxButton button, MessageBoxImage icon);
+    }
+}
