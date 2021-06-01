0b23a4a27b7a40a3090c7e543ff88017b9a0da2d
truongaxin123 <truongaxin123@gmail.com>
2021-06-01 16:36:17 +0700

    add enum semester

---
+++ cs4rsa/BasicData/Semester.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa.BasicData
+{
+    public enum Semester
+    {
+        FirstSemester = 70,
+        SecondSemester = 71,
+        SummerSemester = 73
+    }
+}
