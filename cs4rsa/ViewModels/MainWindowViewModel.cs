566b021971d4d87d202791880db7c6a6dffe7b7c
truongaxin123 <truongaxin123@gmail.com>
2021-06-09 23:23:08 +0700

    add mainwindow vm

---
+++ cs4rsa/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cs4rsa.Dialogs.DialogService;
+using cs4rsa.Dialogs.Implements;
+
+namespace cs4rsa.ViewModels
+{
+    class MainWindowViewModel
+    {
+        public RelayCommand SessionManagerCommand;
+
+        public MainWindowViewModel()
+        {
+            SessionManagerCommand = new RelayCommand(OnOpenSessionManager, () => true);
+        }
+
+        private void OnOpenSessionManager(object obj)
+        {
+            
+        }
+    }
+}
