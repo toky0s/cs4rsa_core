5181fce61eeead5683c38a2146b2833b0a546a36
truongaxin123 <truongaxin123@gmail.com>
2021-06-12 11:44:06 +0700

    edit method

--- cs4rsa/Views/ScheduleTable.xaml.cs
+++ cs4rsa/Views/ScheduleTable.xaml.cs
@@ -22,45 +22,5 @@ namespace cs4rsa.Views
             DataGridFirstPhase.ItemsSource = scheduleTableViewModel.Schedule1;
             DataGridSecondPhase.ItemsSource = scheduleTableViewModel.Schedule2;
         }
-
-        /// <summary>
-        /// Phương thức này thực hiện tô màu cho bảng sau khi render xong dữ liệu
-        /// </summary>
-        /// <param name="scheduleRows"></param>
-        /// <param name="dataGrid"></param>
-        private void PaintColor(ObservableCollection<ScheduleRow> scheduleRows, DataGrid dataGrid)
-        {
-
-        }
-    }
-}
-
-namespace cs4rsa.Converters
-{
-    public class ShortedTimeViewConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            ShortedTime shortedTime = value as ShortedTime;
-            return shortedTime.NewTime.ToString("HH:mm");
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class ClassGroupModelConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return value is ClassGroupModel classGroupModel ? classGroupModel.Name : null;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
