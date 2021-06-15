ed775a88d93b0b1c01151638c6b97d9070f44d29
truongaxin123 <truongaxin123@gmail.com>
2021-06-15 11:05:48 +0700

    add basic datas

---
+++ cs4rsa/BasicData/BetweenPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa.BasicData
+{
+    /// <summary>
+    /// Đại diện cho khoảng giữa hai giờ đầu và hai giờ sau của một buổi trong ngày
+    /// </summary>
+    class BetweenPoint
+    {
+        public Place Place1;
+        public Place Place2;
+        public GoRange GoRange;
+    }
+}
---
+++ cs4rsa/BasicData/PlaceConflictFinder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa.BasicData
+{
+
+    /// <summary>
+    /// Trình tìm kiếm xung đột hai giờ đầu và hai giờ cuối của hai ClassGroup
+    /// </summary>
+    class PlaceConflictFinder
+    {
+        private static readonly List<BetweenPoint> BetweenPointCollection = new List<BetweenPoint>();
+        private readonly ClassGroup _classGroup1;
+        private readonly ClassGroup _classGroup2;
+
+        public PlaceConflictFinder(ClassGroup classGroup1, ClassGroup classGroup2)
+        {
+            // init between point collection
+
+            _classGroup1 = classGroup1;
+            _classGroup2 = classGroup2;
+        }
+    }
+}
