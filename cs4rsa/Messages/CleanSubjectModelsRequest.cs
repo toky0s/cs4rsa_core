0719f725a4a1a94e1effb2401556b76cf9998fb8
truongaxin123 <truongaxin123@gmail.com>
2021-06-17 00:16:15 +0700

    add message

---
+++ cs4rsa/Messages/CleanSubjectModelsRequest.cs
@@ -0,0 +1,21 @@
+﻿using cs4rsa.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa.Messages
+{
+    /// <summary>
+    /// publisher: SubjectImporter
+    /// subcriber: SearchViewModel
+    /// work: Xoá tất cả SubjectModel trong SearchViewModel.
+    /// </summary>
+    public class CleanSubjectModelsRequest : Cs4rsaMessage
+    {
+        public CleanSubjectModelsRequest(object source) : base(source)
+        {
+        }
+    }
+}
