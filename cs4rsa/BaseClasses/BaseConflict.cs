0d2b94b2a5c9d17e3e0a223a79b105ef22d8b946
truongaxin123 <truongaxin123@gmail.com>
2021-06-20 23:03:03 +0700

    add base conflict

---
+++ cs4rsa/BaseClasses/BaseConflict.cs
@@ -0,0 +1,40 @@
+﻿using cs4rsa.BasicData;
+using cs4rsa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa.BaseClasses
+{
+    /// <summary>
+    /// Mọi Conflict đều phải kế thừa từ lớp này
+    /// </summary>
+    public abstract class BaseConflict
+    {
+        protected ClassGroup _classGroup1;
+        protected ClassGroup _classGroup2;
+
+        public ClassGroup FirstClassGroup { get => _classGroup1; private set => _classGroup1 = value; }
+        public ClassGroup SecondClassGroup { get => _classGroup2; private set => _classGroup2 = value; }
+
+        public BaseConflict(ClassGroup classGroup1, ClassGroup classGroup2)
+        {
+            _classGroup1 = classGroup1;
+            _classGroup2 = classGroup2;
+        }
+
+        public BaseConflict(ClassGroupModel classGroup1, ClassGroupModel classGroup2)
+        {
+            _classGroup1 = classGroup1.classGroup;
+            _classGroup2 = classGroup2.classGroup;
+        }
+
+        protected static bool CanConflictPhase(Phase phase1, Phase phase2)
+        {
+            if (phase1 == Phase.ALL || phase2 == Phase.ALL) return true;
+            return phase1 == phase2;
+        }
+    }
+}
