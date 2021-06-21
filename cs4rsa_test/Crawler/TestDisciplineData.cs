140aea7987683fb7a2b89b14550f887b7bfd8142
truongaxin123 <truongaxin123@gmail.com>
2021-06-21 23:52:03 +0700

    add test

--- cs4rsa_test/Crawler/TestDisciplineData.cs
+++ cs4rsa_test/Crawler/TestDisciplineData.cs
@@ -8,5 +8,18 @@ using cs4rsa.Crawler;
 
 namespace cs4rsa.Tests.Crawler
 {
-    
+    [TestFixture]
+    class TestDisciplineData
+    {
+        /// <summary>
+        /// Số lượng môn học có trong học kỳ hiện tại.
+        /// Lúc thực hiện đoạn test này là học kỳ 73 thuộc kỳ Hè
+        /// </summary>
+        [Test]
+        public void GetSoLuongMonHoc()
+        {
+            int expected = DisciplineData.GetNumberOfSubjects();
+            Assert.AreEqual(expected, 397);
+        }
+    }
 }
