edead93bd9c03bfcb397d19a02d8e10c9c4ef138
truongaxin123 <truongaxin123@gmail.com>
2021-06-22 23:08:28 +0700

    add message

---
+++ cs4rsa/Messages/UpdateSuccessMessage.cs
@@ -0,0 +1,16 @@
+﻿using cs4rsa.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa.Messages
+{
+    public class UpdateSuccessMessage : Cs4rsaMessage
+    {
+        public UpdateSuccessMessage(object source) : base(source)
+        {
+        }
+    }
+}
