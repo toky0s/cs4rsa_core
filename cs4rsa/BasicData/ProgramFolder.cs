485fddb9c6d75e964933fdf9eb30a91baa067ad8
truongaxin123 <truongaxin123@gmail.com>
2021-06-24 23:47:26 +0700

    add basic data

---
+++ cs4rsa/BasicData/ProgramFolder.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa.BasicData
+{
+    /// <summary>
+    /// Đại diện cho hình thức học của một Folder, có thể là bắt buộc
+    /// có thể là Chọn n trong k môn có trong folder.
+    /// Compulisory: Bắt buộc
+    /// AllowSelection: Cho phép chọn n trong k môn
+    /// NonDefine: Không có định nghĩa về phần này
+    /// </summary>
+    public enum StudyMode
+    {
+        Compulsory,
+        AllowSelection,
+        NonDefine
+    }
+
+    class ProgramFolder
+    {
+        private string _name;
+        private List<ProgramFolder> _childProgramFolders;
+        private List<ProgramSubject> _programSubject;
+        private bool _isRoot;
+        private string _rawHtml;
+        private StudyMode _studyMode;
+
+        public string Name { get => _name; private set => _name = value; }
+        public List<ProgramFolder> ChildProgramFolder { get => _childProgramFolders; private set => _childProgramFolders = value; }
+        public List<ProgramSubject> ProgramSubjects { get => _programSubject; private set => _programSubject = value; }
+        public bool IsRoot { get => _isRoot; private set => _isRoot = value; }
+        public StudyMode StudyMode { get => _studyMode; private set => _studyMode = value; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name">Tên node.</param>
+        /// <param name="studyMode">Bắt buộc, chọn n trong k hoặc không có.</param>
+        /// <param name="isRoot">Là node gốc.</param>
+        /// <param name="nodeId">Id node.</param>
+        /// <param name="childOfNode">Id của node cha.</param>
+        /// <param name="rawHtml">Html gốc.</param>
+        public ProgramFolder(string name, StudyMode studyMode, bool isRoot, string nodeId, string childOfNode,string rawHtml)
+        {
+            _name = name;
+            _studyMode = studyMode;
+            _isRoot = isRoot;
+            _rawHtml = rawHtml;
+        }
+
+        /// <summary>
+        /// Kiểm tra folder này đã hoàn tất hay chưa,
+        /// folder này hoàn tất hay không phụ thuộc hoàn thoàn vào
+        /// việc các môn và các folder bên trong đã hoàn tất hay chưa.
+        /// </summary>
+        /// <returns></returns>
+        private bool IsComplete()
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Kiểm tra một mã môn có tồn tại trong folder này hay không.
+        /// Nó sẽ đệ quy qua tất cả các folder con bên trong.
+        /// </summary>
+        /// <param name="subjectCode"></param>
+        /// <returns></returns>
+        public bool IsExistsSubject(string subjectCode)
+        {
+            return false;
+        }
+    }
+}
---
+++ cs4rsa/BasicData/ProgramSubject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace cs4rsa.BasicData
+{
+    /// <summary>
+    /// Đại diện cho tình trạng học của một môn như Đã hoàn tất, 
+    /// Đã đang học-Chưa có điểm, hoặc các môn chưa học (và chưa đăng ký)
+    /// </summary>
+    public enum StudyState
+    {
+        Completed,
+        NoHavePoint,
+        UnLearned
+    }
+
+    /// <summary>
+    /// Đại diện cho một Row của trong bảng chương trình học.
+    /// Chứa thông tin cơ bản của Môn có trong chương trình.
+    /// </summary>
+    public class ProgramSubject
+    {
+        private string _subjectCode;
+        private string _subjectName;
+        private string _studyUnit;
+        private string _studyUnitType;
+        // Lần lượt là các môn tiên quyết và song hành
+        private List<ProgramSubject> _prerequisiteSubjects;
+        private List<ProgramSubject> _parallelSubject;
+        private StudyState _studyState;
+
+        public ProgramSubject(string subjectCode, string subjectName, string studyUnit, string studyUnitType,
+            List<ProgramSubject> prerequisiteSubjects, List<ProgramSubject> parallelSubject, StudyState studyState)
+        {
+            _subjectCode = subjectCode;
+            _subjectName = subjectName;
+            _studyUnit = studyUnit;
+            _studyUnitType = studyUnitType;
+            _prerequisiteSubjects = prerequisiteSubjects;
+            _parallelSubject = parallelSubject;
+            _studyState = studyState;
+        }
+    }
+}
