5059f232a74cd5ca9c89df70945fe3a528e12a42
truongaxin123 <truongaxin123@gmail.com>
2021-06-25 23:26:55 +0700

    add interfaces

---
+++ cs4rsa/Interfaces/IProgramNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa.Interfaces
+{
+    public interface IProgramNode
+    {
+        string GetIdNode();
+        string GetChildOfNode();
+    }
+}
---
+++ cs4rsa/Interfaces/IProgramSubjectSaver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa.Interfaces
+{
+    public interface IProgramSubjectSaver
+    {
+        void Save(string courseid, string subjectCode, List<string> prerequisites,
+            List<string> parallels);
+    }
+}
