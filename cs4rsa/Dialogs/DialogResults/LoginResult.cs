40bb26e98227821a3a2b92cc91fcc844a0d982f6
truongaxin123 <truongaxin123@gmail.com>
2021-06-27 23:56:47 +0700

    add dialog

---
+++ cs4rsa/Dialogs/DialogResults/LoginResult.cs
@@ -0,0 +1,14 @@
+﻿using cs4rsa.BasicData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa.Dialogs.DialogResults
+{
+    class LoginResult
+    {
+        public StudentInfo StudentInfo;
+    }
+}
---
+++ cs4rsa/Dialogs/Implements/LoginViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cs4rsa.BasicData;
+using cs4rsa.Dialogs.DialogResults;
+using cs4rsa.Dialogs.DialogService;
+using cs4rsa.Database;
+
+namespace cs4rsa.Dialogs.Implements
+{
+    class LoginViewModel: DialogViewModelBase<LoginResult>
+    {
+        private ObservableCollection<StudentInfo> _studentInfos = new ObservableCollection<StudentInfo>();
+        public ObservableCollection<StudentInfo> StudentInfos
+        {
+            get
+            {
+                return _studentInfos;
+            }
+            set
+            {
+                _studentInfos = value;
+            }
+        }
+
+        private StudentInfo _selectedStudentInfo;
+        public StudentInfo SelectedStudentInfo
+        {
+            get
+            {
+                return _selectedStudentInfo;
+            }
+            set
+            {
+                _selectedStudentInfo = value;
+            }
+        }
+
+        public LoginViewModel()
+        {
+            List<StudentInfo> studentInfos = Cs4rsaDataView.GetStudentInfos();
+            foreach (StudentInfo info in studentInfos)
+            {
+                _studentInfos.Add(info);
+            }
+        }
+    }
+}
