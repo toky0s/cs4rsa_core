467fb5c4a8505fe1acd0312a69ec090490a8956c
truongaxin123 <truongaxin123@gmail.com>
2021-06-29 19:05:34 +0700

    add basic data

---
+++ cs4rsa/BasicData/PreParSubject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa.BasicData
+{
+    /// <summary>
+    /// Đại diện cho các môn tiên quyết và môn song hành của một môn nào đó.
+    /// </summary>
+    public class PreParSubject
+    {
+        private List<string> _preSubjects;
+        private List<string> _parSubjects;
+        public List<string> PreSubjects => _preSubjects;
+        public List<string> ParSubjects => _parSubjects;
+        public PreParSubject(List<string> preSubjects, List<string> parSubjects)
+        {
+            _preSubjects = preSubjects;
+            _parSubjects = parSubjects;
+        }
+    }
+}
---
+++ cs4rsa/BasicData/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa.BasicData
+{
+    public class Student
+    {
+        private StudentInfo _info;
+        private ProgramDiagram _diagram;
+
+        public StudentInfo Info => _info;
+        public ProgramDiagram ProgramDiagram => _diagram;
+
+        public Student(StudentInfo info, ProgramDiagram diagram)
+        {
+            _info = info;
+            _diagram = diagram;
+        }
+    }
+}
