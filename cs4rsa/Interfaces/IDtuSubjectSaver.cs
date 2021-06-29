d4b0f3b729ebf70958603934a44ad6c34492f10f
truongaxin123 <truongaxin123@gmail.com>
2021-06-29 19:07:17 +0700

    add interfaces

---
+++ cs4rsa/Interfaces/IDtuSubjectSaver.cs
@@ -0,0 +1,15 @@
+﻿using cs4rsa.BasicData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa.Interfaces
+{
+
+    public interface IDtuSubjectSaver
+    {
+        void Save(ProgramSubject programSubject);
+    }
+}
---
+++ cs4rsa/Interfaces/ISaver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa.Interfaces
+{
+    /// <summary>
+    /// Implement của interface này thực hiện thao tác trên một dữ liệu
+    /// nào đó của crawler vào database. Các crawler muốn save data
+    /// phải implement interface này chứ không sử dụng trực tiếp
+    /// Cs4rsaDataView hoặc Cs4rsaDataEdit.
+    /// </summary>
+    /// <typeparam name="T">Một BasicData hoặc một Model.</typeparam>
+    public interface ISaver<T>
+    {
+        void Save(T obj);
+    }
+}
