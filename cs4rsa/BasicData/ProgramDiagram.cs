af869d4fadf0a6c0763f37a7925fa716ad340ed3
truongaxin123 <truongaxin123@gmail.com>
2021-06-29 19:05:48 +0700

    fix

--- cs4rsa/BasicData/ProgramDiagram.cs
+++ cs4rsa/BasicData/ProgramDiagram.cs
@@ -10,121 +10,20 @@ namespace cs4rsa.BasicData
     /// ** Đây là class quan trọng giúp thao tác với các Node **
     /// ** Muốn thao tác với một node phải thông qua class này **
     /// </summary>
-    class ProgramDiagram
+    public class ProgramDiagram
     {
-        private List<ProgramFolder> _folderNodes;
-        private List<ProgramSubject> _subjectNodes;
-        private List<IProgramNode> _programNodes;
+        private ProgramFolder _outlineRoot;
+        private ProgramFolder _physicalEducationRoot;
+        private ProgramFolder _industryOutlineRoot;
+        private ProgramFolder _specializedRoot;
 
-        public ProgramDiagram(List<ProgramFolder> folderNode, List<ProgramSubject> subjectNode)
+        public ProgramDiagram(ProgramFolder outlineRoot, ProgramFolder physicalEducationRoot,
+                              ProgramFolder industryOutlineRoot, ProgramFolder specializedRoot)
         {
-            _subjectNodes = subjectNode;
-            _folderNodes = DivChildNode(folderNode, subjectNode);
-            _programNodes.AddRange(_folderNodes);
-            _programNodes.AddRange(_subjectNodes);
-        }
-
-        /// <summary>
-        /// Copy các ProgramSubject con vào các ProgramFolder.
-        /// </summary>
-        /// <param name="nodes">Danh sách tất cả các node bao gồm cả subject và folder.</param>
-        private List<ProgramFolder> DivChildNode(List<ProgramFolder> folderNodes, List<ProgramSubject> subjectNodes)
-        {
-            List<ProgramFolder> programFolders = new List<ProgramFolder>();
-            foreach (ProgramFolder node in folderNodes)
-            {
-                List<ProgramSubject> subjects = GetChildProgramSubject(node, subjectNodes);
-                node.AddNodes(subjects);
-                programFolders.Add(node);
-            }
-            return programFolders;
-        }
-
-        private List<ProgramSubject> GetChildProgramSubject(ProgramFolder parrent, List<ProgramSubject> subjects)
-        {
-            List<ProgramSubject> childSubjects = new List<ProgramSubject>();
-            foreach (ProgramSubject subject in subjects)
-            {
-                if (subject.GetChildOfNode() == parrent.GetIdNode())
-                    childSubjects.Add(subject);
-            }
-            return childSubjects;
-        }
-
-        //public bool IsCompletedNode(IProgramNode node)
-        //{
-        //    if (node is ProgramSubject)
-        //    {
-        //        ProgramSubject subject = node as ProgramSubject;
-        //        return subject.IsCompleted();
-        //    }
-        //    else
-        //    {
-        //        ProgramFolder folderNode = node as ProgramFolder;
-        //        bool flag = true;
-        //        List<IProgramNode> childNodes = GetChildNodes(node, _programNodes);
-        //        foreach (IProgramNode n in childNodes)
-        //        {
-        //            if (n is ProgramSubject)
-        //            {
-        //                ProgramSubject subject = n as ProgramSubject;
-        //                if (subject.IsUnLearn())
-        //                    flag = flag && false;
-        //            }
-        //            else
-        //            {
-        //                if (IsCompletedNode(n))
-        //                    flag = flag && false;
-        //            }
-        //        }
-                
-        //    }
-        //}
-
-        private List<IProgramNode> GetFolderNodesInCollection(List<IProgramNode> nodes)
-        {
-            List<IProgramNode> folderNodes = new List<IProgramNode>();
-            foreach (IProgramNode node in nodes)
-            {
-                if (node is ProgramFolder)
-                    folderNodes.Add(node);
-            }
-            return folderNodes;
-        }
-
-
-        /// <summary>
-        /// Kiểm tra một danh sách các node xem có chứa một folder node nào đó không.
-        /// </summary>
-        /// <param name="nodes"></param>
-        /// <returns></returns>
-        private bool IsHaveFolderNodeIn(List<IProgramNode> nodes)
-        {
-            foreach (IProgramNode node in nodes)
-            {
-                if (node is ProgramFolder)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-
-        /// <summary>
-        /// Trả về một List chứa các child node của một node.
-        /// </summary>
-        /// <param name="node"></param>
-        /// <returns></returns>
-        private List<IProgramNode> GetChildNodes(IProgramNode parrent, List<IProgramNode> nodes)
-        {
-            List<IProgramNode> childNodes = new List<IProgramNode>();
-            foreach(IProgramNode n in nodes)
-            {
-                if (n.GetChildOfNode() == parrent.GetIdNode())
-                    childNodes.Add(n);
-            }
-            return childNodes;
+            _outlineRoot = outlineRoot;
+            _physicalEducationRoot = physicalEducationRoot;
+            _industryOutlineRoot = industryOutlineRoot;
+            _specializedRoot = specializedRoot;
         }
     }
 }
--- cs4rsa/BasicData/ProgramSubject.cs
+++ cs4rsa/BasicData/ProgramSubject.cs
@@ -31,6 +31,7 @@ namespace cs4rsa.BasicData
         private List<string> _prerequisiteSubjects;
         private List<string> _parallelSubjects;
         private StudyState _studyState;
+        private string _courseId;
 
         public string Id => _id;
         public string ChildOfNode => _childOfNode;
@@ -41,9 +42,10 @@ namespace cs4rsa.BasicData
         public List<string> PrerequisiteSubjects => _prerequisiteSubjects;
         public List<string> ParallelSubjects => _parallelSubjects;
         public StudyState StudyState => _studyState;
+        public string CourseId => _courseId;
 
         public ProgramSubject(string id, string childOfNode, string subjectCode, string subjectName, string studyUnit, StudyUnitType studyUnitType,
-            List<string> prerequisiteSubjects, List<string> parallelSubject, StudyState studyState)
+            List<string> prerequisiteSubjects, List<string> parallelSubject, StudyState studyState, string courseId)
         {
             _id = id;
             _childOfNode = childOfNode;
@@ -54,19 +56,7 @@ namespace cs4rsa.BasicData
             _prerequisiteSubjects = prerequisiteSubjects;
             _parallelSubjects = parallelSubject;
             _studyState = studyState;
-        }
-
-        public ProgramSubject(ProgramSubject programSubject)
-        {
-            _id = programSubject.Id;
-            _childOfNode = programSubject.ChildOfNode;
-            _subjectCode = programSubject.SubjectCode;
-            _subjectName = programSubject.SubjectName;
-            _studyUnit = programSubject.StudyUnit;
-            _studyUnitType = programSubject.StudyUnitType;
-            _prerequisiteSubjects = programSubject.PrerequisiteSubjects;
-            _parallelSubjects = programSubject.ParallelSubjects;
-            _studyState = programSubject.StudyState;
+            _courseId = courseId;
         }
 
         public string GetChildOfNode()
