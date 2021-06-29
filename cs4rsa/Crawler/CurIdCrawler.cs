34a7eba3167b00799c978bf8d383560b2769daa2
truongaxin123 <truongaxin123@gmail.com>
2021-06-29 19:06:16 +0700

    add crawler

---
+++ cs4rsa/Crawler/CurIdCrawler.cs
@@ -0,0 +1,26 @@
+﻿using cs4rsa.Helpers;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace cs4rsa.Crawler
+{
+    public class CurIdCrawler
+    {
+        public static string GetCurId(string specialString)
+        {
+            string t = Helpers.Helpers.GetTimeFromEpoch();
+            string url = $"https://mydtu.duytan.edu.vn/Modules/curriculuminportal/ajax/LoadChuongTrinhHoc.aspx?t={t}&studentidnumber={specialString}";
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+            HtmlNode docNode = doc.DocumentNode;
+            HtmlNode scriptNode = docNode.SelectSingleNode("//td/script");
+            string content = scriptNode.InnerText;
+
+            Regex regex = new Regex("curid=[0-9]*");
+            Match matching = regex.Match(content);
+            string matchString = matching.Value.ToString();
+            string curid = matchString.Split(new char[] { '=' })[1];
+            return curid;
+        }
+    }
+}
