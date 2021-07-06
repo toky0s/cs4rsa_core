4d9c67bc89dffdb15aeb4f26b69ab853c05f1da9
truongaxin123 <truongaxin123@gmail.com>
2021-07-07 00:03:29 +0700

    add enums

---
+++ cs4rsa/Enums/ImplementType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa.Enums
+{
+    /// <summary>
+    /// Mô tả cho tình trạng triển khai của một class group.
+    /// </summary>
+    public enum ImplementType
+    {
+        Started,
+        Unstart,
+        Canceled
+    }
+}
