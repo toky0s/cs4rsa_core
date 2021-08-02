8580cc30b456a1c6034644f8ed0affb68d954557
truongaxin123 <truongaxin123@gmail.com>
2021-08-02 23:38:26 +0700

    add settings

---
+++ cs4rsa/Settings/SettingsWriter.cs
@@ -0,0 +1,50 @@
+﻿using cs4rsa.Crawler;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa.Settings
+{
+    public class SettingsWriter
+    {
+        private static readonly string _settingsFileName = "cs4rsa_settings.json";
+        private static SettingsWriter _instance = new SettingsWriter();
+
+        private static HomeCourseSearch _homeCourseSearch = HomeCourseSearch.GetInstance();
+
+        private static Cs4rsaSetting _defaultSettings = new Cs4rsaSetting
+        {
+            CurrentSemesterValue = _homeCourseSearch.CurrentSemesterValue,
+            CurrentYearValue = _homeCourseSearch.CurrentYearValue
+        };
+
+        private SettingsWriter()
+        {
+
+        }
+
+        public static SettingsWriter GetInstance()
+        {
+            return _instance;
+        }
+
+        public void Save(Cs4rsaSetting cs4RsaSetting)
+        {
+            string jsonString = JsonConvert.SerializeObject(cs4RsaSetting);
+            File.WriteAllText(_settingsFileName, jsonString);
+        }
+
+        public void InitSettings()
+        {
+            if (!File.Exists(_settingsFileName))
+            {
+                string jsonString = JsonConvert.SerializeObject(_defaultSettings);
+                File.WriteAllText(_settingsFileName, jsonString);
+            }
+        }
+    }
+}
