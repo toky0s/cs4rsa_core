d36bcb3469b190a780bfe5e23e2b0aab624617ad
truongaxin123 <truongaxin123@gmail.com>
2021-08-02 23:39:12 +0700

    add message

---
+++ cs4rsa/Messages/UpdateSubjectDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cs4rsa.BaseClasses;
+
+namespace cs4rsa.Messages
+{
+    public class UpdateSubjectDatabase : Cs4rsaMessage
+    {
+        public UpdateSubjectDatabase(object source) : base(source)
+        {
+        }
+    }
+}
