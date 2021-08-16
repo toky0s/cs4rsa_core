26f05d2e0be7a7dfc12745180adbda2ffa9fcacb
truongaxin123 <truongaxin123@gmail.com>
2021-08-16 19:04:12 +0700

    edit dialog view

--- cs4rsa/Dialogs/DialogViews/AutoSortSubjectLoadUC.xaml.cs
+++ cs4rsa/Dialogs/DialogViews/AutoSortSubjectLoadUC.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace cs4rsa.Dialogs.DialogViews
 {
     /// <summary>
-    /// Interaction logic for LoginWindow.xaml
+    /// Interaction logic for AutoSortSubjectLoadUC.xaml
     /// </summary>
-    public partial class LoginDialogWindow : Window
+    public partial class AutoSortSubjectLoadUC : UserControl
     {
-        public LoginDialogWindow()
+        public AutoSortSubjectLoadUC()
         {
             InitializeComponent();
         }
