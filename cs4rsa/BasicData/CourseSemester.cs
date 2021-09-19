b85c761aff4043a0e0e45ce868ea631ec4c5f9b1
truongaxin123 <truongaxin123@gmail.com>
2021-09-19 20:05:06 +0700

    edit basic data

---
+++ cs4rsa/BasicData/CourseSemester.cs
@@ -0,0 +1,18 @@
+﻿namespace cs4rsa.BasicData
+{
+    /// <summary>
+    /// Class này đại diện cho thông tin của một học kỳ, đóng vai trò
+    /// quan trọng trong việc xác định được course cần thiết trong các
+    /// crawler liên quan tới cào thông tin môn học và bảng điểm sinh viên.
+    /// </summary>
+    public class CourseSemester
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public CourseSemester(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
