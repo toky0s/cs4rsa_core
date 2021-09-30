7d4d8e2c82381af428ee0bbe024e1745ca856daf
truongaxin123 <truongaxin123@gmail.com>
2021-09-30 23:38:28 +0700

    add teacher crawler service

---
+++ TeacherCrawlerService1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Cs4rsaDatabaseService.DataProviders;
+using Cs4rsaDatabaseService.Models;
+using TeacherCrawlerService1.Crawlers;
+
+namespace TeacherCrawlerService1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Cs4rsaDbContext cs4RsaDbContext = new Cs4rsaDbContext())
+            {
+                cs4RsaDbContext.Database.EnsureCreated();
+                string url = @"http://courses.duytan.edu.vn/Sites/Home_ChuongTrinhDaoTao.aspx?p=home_lecturerdetail&timespan=71&intructorid=221111108&classid=132070&academicleveltypeid=&curriculumid=";
+                TeacherCrawler teacherCrawler = new TeacherCrawler(url, cs4RsaDbContext);
+                Teacher teacher = teacherCrawler.Crawl();
+                Console.WriteLine(teacher.Name);
+            }
+        }
+    }
+}
