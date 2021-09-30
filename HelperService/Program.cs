36589b6fe054f550e5a358f8e212b886c7759b3c
truongaxin123 <truongaxin123@gmail.com>
2021-09-30 23:35:02 +0700

    add helper service

---
+++ HelperService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelperService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
