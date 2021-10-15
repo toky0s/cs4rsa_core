94ef730bd1c4f4d18fdfa6921bb45882e351cfa0
truongaxin123 <truongaxin123@gmail.com>
2021-10-15 11:40:30 +0700

    edit Curriculum Crawler Service

--- CurriculumCrawlerService/Program.cs
+++ CurriculumCrawlerService/Program.cs
@@ -6,7 +6,7 @@ namespace CurriculumCrawlerService
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Curriculum Crawler Service");
         }
     }
 }
