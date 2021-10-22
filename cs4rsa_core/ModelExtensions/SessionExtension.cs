be2e94f43e20578f043dda0ba3bc06bff183ece7
truongaxin123 <truongaxin123@gmail.com>
2021-10-23 00:13:11 +0700

    init model extensions

---
+++ cs4rsa_core/ModelExtensions/SessionExtension.cs
@@ -0,0 +1,25 @@
+﻿using CourseSearchService.Crawlers.Interfaces;
+using Cs4rsaDatabaseService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa_core.ModelExtensions
+{
+    public class SessionExtension
+    {
+        private ICourseCrawler _courseCrawler;
+        public SessionExtension(ICourseCrawler courseCrawler)
+        {
+            _courseCrawler = courseCrawler;
+        }
+
+        public bool IsValid(Session session)
+        {
+            return session.SemesterValue.Equals(_courseCrawler.GetCurrentSemesterValue()) 
+                && session.YearValue.Equals(_courseCrawler.GetCurrentYearValue());
+        }
+    }
+}
