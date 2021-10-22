be323f6883db8dd547052399e33fefeac4e222e4
truongaxin123 <truongaxin123@gmail.com>
2021-10-23 00:13:47 +0700

    init settings

---
+++ cs4rsa_core/Settings/Cs4rsaSetting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace cs4rsa_core.Settings
+{
+    public class Cs4rsaSetting
+    {
+        public string CurrentYearValue { get; set; }
+        public string CurrentSemesterValue { get; set; }
+        public string IsDatabaseCreated { get; set; }
+        public string Version { get; set; }
+        public object this[string propertyName]
+        {
+            get { return this.GetType().GetProperty(propertyName).GetValue(this, null); }
+            set { this.GetType().GetProperty(propertyName).SetValue(this, value, null); }
+        }
+    }
+}
---
+++ cs4rsa_core/Settings/Setting.cs
@@ -0,0 +1,67 @@
+﻿using CourseSearchService.Crawlers.Interfaces;
+using cs4rsa_core.Settings.Interfaces;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace cs4rsa_core.Settings
+{
+    public class Setting : ISetting
+    {
+        public Cs4rsaSetting CurrentSetting { get; set; }
+        public string SettingsFileName { get; set; } = "cs4rsa_settings.json";
+
+        private readonly ICourseCrawler _courseCrawler;
+        public Setting(ICourseCrawler courseCrawler)
+        {
+            _courseCrawler = courseCrawler;
+            Init();
+        }
+
+        public void Init()
+        {
+            Cs4rsaSetting defaultSettings = new Cs4rsaSetting
+            {
+                CurrentSemesterValue = _courseCrawler.GetCurrentSemesterValue(),
+                CurrentYearValue = _courseCrawler.GetCurrentYearValue(),
+                IsDatabaseCreated = "false",
+                Version = "1.1.2"
+            };
+            if (!File.Exists(SettingsFileName))
+            {
+                string jsonString = JsonConvert.SerializeObject(defaultSettings);
+                File.WriteAllText(SettingsFileName, jsonString);
+                CurrentSetting = defaultSettings;
+            }
+            else
+            {
+                using (StreamReader file = File.OpenText(SettingsFileName))
+                using (JsonTextReader reader = new JsonTextReader(file))
+                {
+                    JObject settingJson = (JObject)JToken.ReadFrom(reader);
+                    CurrentSetting = settingJson.ToObject<Cs4rsaSetting>();
+                }
+            }
+        }
+
+        public string Read(string key)
+        {
+            try
+            {
+                return CurrentSetting[key].ToString();
+            }
+            catch
+            {
+                Exception exception = new("Setting is not exist");
+                throw exception;
+            }
+        }
+
+        public void Save()
+        {
+            string jsonString = JsonConvert.SerializeObject(CurrentSetting);
+            File.WriteAllText(SettingsFileName, jsonString);
+        }
+    }
+}
