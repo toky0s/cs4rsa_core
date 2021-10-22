6eb2bbd11f9b311f55fafeb94fb627f40a8d6428
truongaxin123 <truongaxin123@gmail.com>
2021-10-23 00:14:59 +0700

    add viewmodel functions

---
+++ cs4rsa_core/ViewModelFunctions/ClassGroupChoicer.cs
@@ -0,0 +1,65 @@
+﻿using cs4rsa_core.Messages;
+using cs4rsa_core.Models;
+using cs4rsa_core.Dialogs.DialogResults;
+using LightMessageBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa_core.ViewModelFunctions
+{
+    /// <summary>
+    /// Bộ chọn class group. Khi bộ chọn này Start nó thực hiện import các class group Model
+    /// được truyền vào method tới choiced sesion.
+    /// </summary>
+    public static class ClassGroupChoicer
+    {
+        public static void Start(List<ClassGroupModel> classGroupModels)
+        {
+            foreach (ClassGroupModel classGroupModel in classGroupModels)
+            {
+                MessageBus.Default.Publish(new ClassGroupAddedMessage(classGroupModel));
+            }
+        }
+
+
+        /// <summary>
+        /// Phương thức Start này sẽ import các class group có trong subject info data,
+        /// bằng cách kiểm tra và tìm kiếm class group trong các subject model được truyền vào.
+        /// Nếu không tìm thấy class group trong subject model thì nó sẽ không import.
+        /// </summary>
+        /// <param name="subjectModels"></param>
+        /// <param name="subjectInfoDatas"></param>
+        public static void Start(List<SubjectModel> subjectModels, List<SubjectInfoData> subjectInfoDatas)
+        {
+            foreach (SubjectInfoData subjectInfoData in subjectInfoDatas)
+            {
+                SubjectModel subjectModel = GetSubjectModelWithSubjectCode(subjectModels, subjectInfoData.SubjectCode);
+                Choice(subjectModel, subjectInfoData.ClassGroup);
+            }
+        }
+
+        private static void Choice(SubjectModel subjectModel, string classGroupName)
+        {
+            ClassGroupModel classGroupModel = subjectModel.GetClassGroupModelWithName(classGroupName);
+            if (classGroupModel != null)
+            {
+                MessageBus.Default.Publish(new ClassGroupAddedMessage(classGroupModel));
+            }
+        }
+
+        private static SubjectModel GetSubjectModelWithSubjectCode(List<SubjectModel> subjectModels, string subjectCode)
+        {
+            foreach (SubjectModel subject in subjectModels)
+            {
+                if (subject.SubjectCode.Equals(subjectCode))
+                {
+                    return subject;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ cs4rsa_core/ViewModelFunctions/UpdateConflict.cs
@@ -0,0 +1,73 @@
+﻿using ConflictService.DataTypes;
+using cs4rsa_core.Messages;
+using cs4rsa_core.Models;
+using LightMessageBus;
+using SubjectCrawlService1.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace cs4rsa_core.ViewModelFunctions
+{
+    /// <summary>
+    /// Mục đích của class này được tạo ra là để các phần xếp lịch thủ công và xếp lịch tự động
+    /// được sử dụng chung với nhau việc cập nhật các xung đột về thời gian và vị trí học.
+    /// </summary>
+    public static class UpdateConflict
+    {
+        /// <summary>
+        /// Thực hiện bắt cặp tất cả các ClassGroupModel có 
+        /// trong Collection để phát hiện các Conflict Time.
+        /// </summary>
+        public static void UpdateConflictModelCollection(ref ObservableCollection<ConflictModel> conflictModels, ref List<ClassGroupModel> classGroupModels)
+        {
+            conflictModels.Clear();
+            List<SchoolClass> schoolClasses = new List<SchoolClass>();
+            foreach (ClassGroupModel classGroupModel in classGroupModels)
+            {
+                schoolClasses.AddRange(classGroupModel.ClassGroup.SchoolClasses);
+            }
+
+            for (int i = 0; i < schoolClasses.Count; ++i)
+            {
+                for (int k = i + 1; k < schoolClasses.Count; ++k)
+                {
+                    if (schoolClasses[i].ClassGroupName.Equals(schoolClasses[k].ClassGroupName))
+                        continue;
+                    Conflict conflict = new Conflict(schoolClasses[i], schoolClasses[k]);
+                    ConflictTime conflictTime = conflict.GetConflictTime();
+                    if (conflictTime != null)
+                    {
+                        ConflictModel conflictModel = new ConflictModel(conflict);
+                        conflictModels.Add(conflictModel);
+                    }
+                }
+            }
+        }
+
+        public static void UpdatePlaceConflictCollection(ref ObservableCollection<PlaceConflictFinderModel> placeConflictFinderModels, ref List<ClassGroupModel> classGroupModels)
+        {
+            placeConflictFinderModels.Clear();
+            List<SchoolClass> schoolClasses = new List<SchoolClass>();
+            foreach (ClassGroupModel classGroupModel in classGroupModels)
+            {
+                schoolClasses.AddRange(classGroupModel.ClassGroup.SchoolClasses);
+            }
+
+            for (int i = 0; i < schoolClasses.Count; ++i)
+            {
+                for (int k = i + 1; k < schoolClasses.Count; ++k)
+                {
+                    PlaceConflictFinder placeConflict = new PlaceConflictFinder(schoolClasses[i], schoolClasses[k]);
+                    ConflictPlace conflictPlace = placeConflict.GetPlaceConflict();
+                    if (conflictPlace != null)
+                    {
+                        PlaceConflictFinderModel placeConflictModel = new PlaceConflictFinderModel(placeConflict);
+                        placeConflictFinderModels.Add(placeConflictModel);
+                    }
+                }
+            }
+        }
+    }
+}
