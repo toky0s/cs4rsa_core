18f0e530003c8a53606e3b1bf7edf2f29f14b00f
truongaxin123 <truongaxin123@gmail.com>
2021-10-27 19:40:43 +0700

    add services

---
+++ cs4rsa_core/Implements/OpenInBrowser.cs
@@ -0,0 +1,17 @@
+﻿using cs4rsa_core.Interfaces;
+using System.Diagnostics;
+
+namespace cs4rsa_core.Implements
+{
+    public class OpenInBrowser : IOpenInBrowser
+    {
+        public void Open(string url)
+        {
+            _ = Process.Start(new ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true
+            });
+        }
+    }
+}
---
+++ cs4rsa_core/Interfaces/IOpenInBrowser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs4rsa_core.Interfaces
+{
+    public interface IOpenInBrowser
+    {
+        void Open(string url);
+    }
+}
