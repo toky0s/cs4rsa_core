5853a860b4f8108241cfa273602e200d5f52abc8
truongaxin123 <truongaxin123@gmail.com>
2022-01-18 16:29:15 +0700

    edit helper service

---
+++ HelperService/HtmlAgilityPackExtension.cs
@@ -0,0 +1,24 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperService
+{
+    public class HtmlAgilityPackExtension
+    {
+        public static HtmlDocument GetHtmlDocumentWithHttpClient(string url)
+        {
+            using HttpClient client = new();
+            using HttpResponseMessage response = client.GetAsync(url).Result;
+            using HttpContent content = response.Content;
+            string result = content.ReadAsStringAsync().Result;
+            HtmlDocument document = new();
+            document.LoadHtml(result);
+            return document;
+        }
+    }
+}
