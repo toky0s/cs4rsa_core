7899c3730072860a98292155ce63f1082352dbfc
truongaxin123 <truongaxin123@gmail.com>
2022-01-18 16:27:48 +0700

    add course search dll

---
+++ CourseSearchDLL/Crawlers/Interfaces/ICourseCrawler.cs
@@ -0,0 +1,19 @@
+﻿using CourseSearchDLL.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseSearchDLL.Crawlers.Interfaces
+{
+    public interface ICourseCrawler
+    {
+        string GetCurrentSemesterValue();
+        string GetCurrentSemesterInfo();
+        string GetCurrentYearValue();
+        string GetCurrentYearInfo();
+
+        List<CourseYear> GetCourseYears();
+    }
+}
