954b45367b3ba589089012b58a8fa6434ca0b2d6
truongaxin123 <truongaxin123@gmail.com>
2022-01-18 16:28:07 +0700

    edit course search service

--- CourseSearchService/Program.cs
+++ CourseSearchService/Program.cs
@@ -7,8 +7,6 @@ namespace CourseSearchService
     {
         static void Main(string[] args)
         {
-            CourseCrawler courseCrawler = new CourseCrawler();
-            Console.WriteLine(courseCrawler.CurrentYearInfo);
         }
     }
 }
