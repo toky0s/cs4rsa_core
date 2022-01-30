a61e5ff9799b28eb9d8b7ad3db7179370a1d3451
TruongAXin <truongaxin123@gmail.com>
2022-01-30 15:57:52 +0700

    fix typoes

--- cs4rsa_core/Dialogs/DialogResults/SessionManagerResult.cs
+++ cs4rsa_core/Dialogs/DialogResults/SessionManagerResult.cs
@@ -3,17 +3,18 @@
 namespace cs4rsa_core.Dialogs.DialogResults
 {
     /// <summary>
-    /// Là một item trong kết quả trả về của trình quản lý phiên.
+    /// Là một item trong kết quả trả về của Trình Quản lý Phiên.
     /// </summary>
     public class SubjectInfoData
     {
         public string SubjectCode { get; set; }
         public string ClassGroup { get; set; }
         public string SubjectName { get; set; }
+        public string RegisterCode { get; set; }
     }
 
     /// <summary>
-    /// Là kết quả trả về của trình quản lý phiên.
+    /// Là kết quả trả về của Trình Quản lý Phiên.
     /// </summary>
     public class SessionManagerResult
     {
