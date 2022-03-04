b4593063ac2e3ade9579d1935b91b3d113ef1ec2
TruongAXin <truongaxin123@gmail.com>
2022-03-04 19:52:29 +0700

    add comment

--- HelperService/Interfaces/IFolderManager.cs
+++ HelperService/Interfaces/IFolderManager.cs
@@ -1,5 +1,9 @@
 ﻿namespace HelperService.Interfaces
 {
+    /// <summary>
+    /// Class triển khai Interface này sẽ triển khai
+    /// các phương thức thao tác với thư mục.
+    /// </summary>
     public interface IFolderManager
     {
         string CreateFolderIfNotExists(string folderName);
