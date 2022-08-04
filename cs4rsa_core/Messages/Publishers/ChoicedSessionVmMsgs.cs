24e538d7a8a574e05d949635ba49026c312ee8fb
axin <truongaxin123@gmail.com>
2022-08-04 23:34:14 +0700

    [CS0011] Refactor messages

---
+++ cs4rsa_core/Messages/Publishers/ChoicedSessionVmMsgs.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Toolkit.Mvvm.Messaging.Messages;
+using SubjectCrawlService1.Models;
+using System.Collections.Generic;
+
+namespace cs4rsa_core.Messages.Publishers
+{
+    internal sealed class ChoicedSessionVmMsgs
+    {
+        /// <summary>
+        /// Có sự thay đổi danh sách các lớp đã chọn. 
+        /// Điều này sẽ gọi qua ScheduleTableViewModel để cập nhật lại mô phỏng
+        /// </summary>
+        internal sealed class ChoicesChangedMsg : ValueChangedMessage<IEnumerable<ClassGroupModel>>
+        {
+            public ChoicesChangedMsg(IEnumerable<ClassGroupModel> value) : base(value)
+            {
+            }
+        }
+    }
+}
