36c37ad37939b5a52c695d82674d8702f91bec97
VietHoang <viethoang@rikkeisoft.com>
2022-11-03 09:42:41 +0700

    :bug: [CS0048] Add new time index for SPM 300

--- cs4rsa_core/Converters/Controls/ScheduleBlockHeightConverter.cs
+++ cs4rsa_core/Converters/Controls/ScheduleBlockHeightConverter.cs
@@ -11,7 +11,7 @@ namespace cs4rsa_core.Converters.Controls
             double douHeight = (double)values[0];
             int intStartIndex = (int)values[1];
             int intEndIndex = (int)values[2];
-            return douHeight / 15 * (intEndIndex - intStartIndex);
+            return douHeight / 16 * (intEndIndex - intStartIndex);
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
