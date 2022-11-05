f1a93d12b63fbdccffd8ce7a71b26a707a1481ac
axin <truongaxin123@gmail.com>
2022-11-05 11:16:09 +0700

    :bug: [CS0052] Bug bộ sinh xung đột vị trí sinh xung đột sai Phase #78

--- cs4rsa_core/Controls/DayPanel.cs
+++ cs4rsa_core/Controls/DayPanel.cs
@@ -1,18 +1,6 @@
 ﻿using cs4rsa_core.Commons.Models;
-
-using MaterialDesignThemes.Wpf;
-
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Media;
-using System.Windows.Shapes;
 
 namespace cs4rsa_core.Controls
 {
