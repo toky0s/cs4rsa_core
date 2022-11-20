1a0224a0e6c0cad5fa0c642b88733a1984904995
axin <truongaxin123@gmail.com>
2022-11-20 23:41:24 +0700

    :zap: Speed upppp

---
+++ cs4rsa_core/Converters/GetTeacherNameConverter.cs
@@ -0,0 +1,30 @@
+﻿using Cs4rsa.Services.TeacherCrawlerSvc.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Cs4rsa.Converters
+{
+    public class GetTeacherNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                List<TeacherModel> objects = (List<TeacherModel>)value;
+                return objects.Count > 0 ? objects[0].Name : parameter;
+            }
+            return parameter;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
