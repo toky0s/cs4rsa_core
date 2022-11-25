bfc1b6ebd5e3d4eaf1d520588295572d696cb8c2
axin <truongaxin123@gmail.com>
2022-11-25 22:38:02 +0700

    🍠 Improve dev experience

--- cs4rsa_core/Views/MainWindow.xaml.cs
+++ cs4rsa_core/Views/MainWindow.xaml.cs
@@ -1,80 +1,143 @@
-﻿using Cs4rsa.Commons.Enums;
+﻿using CommunityToolkit.Mvvm.Input;
+
+using Cs4rsa.Constants;
 using Cs4rsa.ViewModels;
+
+using MaterialDesignThemes.Wpf;
 
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Cs4rsa.Views
 {
     public partial class MainWindow : Window
     {
-
+        private static readonly int _heightAndWidth = 24;
+        private int _currentMenuItemIndex = -1;
         public MainWindow()
         {
             InitializeComponent();
+            RenderScreen();
+            RenderMainMenu();
+            RenderCompactMenu();
             Goto((int)ScreenIndex.HAND);
         }
 
-        public void LoadPage(ScreenIndex screenIndex)
+        private void RenderScreen()
         {
-            Goto((int)screenIndex);
+            foreach (var item in ViewConstants.CREDIZ_MENU_ITEMS)
+            {
+                CredizTransitioner.Items.Add(item.CredizScreen.Screen);
+            }
+        }
+
+        public void RenderMainMenu()
+        {
+
+            Thickness textBlockMargin = new(30, 0, 0, 0);
+            Thickness panelMargin = new(10, 0, 0, 0);
+            foreach (CredizMenuItem item in ViewConstants.CREDIZ_MENU_ITEMS)
+            {
+                ListBoxItem listBoxItem = new();
+
+                PackIcon icon = new()
+                {
+                    Kind = item.IconKind,
+                    Width = _heightAndWidth,
+                    Height = _heightAndWidth
+                };
+
+                TextBlock textBlock = new()
+                {
+                    Text = item.MenuName,
+                    Margin = textBlockMargin,
+                    VerticalAlignment = VerticalAlignment.Center
+                };
+
+                StackPanel stackPanel = new()
+                {
+                    Margin = panelMargin,
+                    Orientation = Orientation.Horizontal
+                };
+
+                stackPanel.Children.Add(icon);
+                stackPanel.Children.Add(textBlock);
+
+                listBoxItem.Content = stackPanel;
+                ListViewMenu.Items.Add(listBoxItem);
+            }
+        }
+
+        public void RenderCompactMenu()
+        {
+            foreach (CredizMenuItem item in ViewConstants.CREDIZ_MENU_ITEMS)
+            {
+                PackIcon icon = new()
+                {
+                    Kind = item.IconKind,
+                    Width = _heightAndWidth,
+                    Height = _heightAndWidth
+                };
+
+                Button button = new()
+                {
+                    Margin = new Thickness(16, 0, 0, 0),
+                    Content = icon,
+                    ToolTip = item.MenuName,
+                    Foreground = Brushes.White,
+                    Style = (Style)FindResource("MaterialDesignToolButton"),
+                    Command = new RelayCommand(() => Goto((int)item.CredizScreen.Index))
+                };
+
+                CompactMenu.Children.Add(button);
+            }
         }
 
         #region Util functions
         private void Goto(int index)
         {
-            if (ListViewMenu.SelectedIndex != index)
+            PackIcon icon;
+            Button button;
+            if (index != _currentMenuItemIndex && _currentMenuItemIndex != -1)
             {
-                ListViewMenu.SelectedIndex = index;
+                icon = new()
+                {
+                    Kind = ViewConstants.CREDIZ_MENU_ITEMS[_currentMenuItemIndex].IconKind,
+                    Width = _heightAndWidth,
+                    Height = _heightAndWidth
+                };
+                button = CompactMenu.Children[_currentMenuItemIndex] as Button;
+                button.Content = icon;
             }
-            MainWindowViewModel mainWindowViewModel = DataContext as MainWindowViewModel;
-            mainWindowViewModel.SelectedIndex = index;
+
+            icon = new()
+            {
+                Kind = ViewConstants.CREDIZ_MENU_ITEMS[index].IconKindOnSelected,
+                Width = _heightAndWidth,
+                Height = _heightAndWidth
+            };
+            ListViewMenu.SelectedIndex = index;
+            button = CompactMenu.Children[index] as Button;
+            button.Content = icon;
+
+            CredizTransitioner.SelectedIndex = index;
+            _currentMenuItemIndex = index;
         }
         #endregion
 
         #region Event handlers
         private async void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            ListView listView = sender as ListView;
+            ListBox listView = sender as ListBox;
             Goto(listView.SelectedIndex);
-            if (listView.SelectedIndex == (int)ScreenIndex.LECTURE)
+            if (listView.SelectedIndex == (int)ScreenIndex.TEACHER)
             {
-                await (Lecture.DataContext as LectureViewModel).LoadTeachers();
+                LectureViewModel lecture = (LectureViewModel)(CredizTransitioner.Items[listView.SelectedIndex] as Lecture).DataContext;
+                await lecture.LoadTeachers();
             }
             // Close Drawer
-            MaterialDesignThemes.Wpf.DrawerHost.CloseDrawerCommand.Execute(null, null);
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            LoadPage(ScreenIndex.HOME);
-        }
-        #endregion
-
-        #region Go to menus
-        private void GotoHome(object sender, RoutedEventArgs e)
-        {
-            Goto((int)ScreenIndex.HOME);
-        }
-
-        private void GotoAccount(object sender, RoutedEventArgs e)
-        {
-            Goto((int)ScreenIndex.ACCOUNT);
-        }
-
-        private void GotoXepLichThuCong(object sender, RoutedEventArgs e)
-        {
-            Goto((int)ScreenIndex.HAND);
-        }
-
-        private void GotoXepLichTuDong(object sender, RoutedEventArgs e)
-        {
-            Goto((int)ScreenIndex.AUTO);
-        }
-
-        private void GotoThongTin(object sender, RoutedEventArgs e)
-        {
-            Goto((int)ScreenIndex.INFO);
+            DrawerHost.CloseDrawerCommand.Execute(null, null);
         }
         #endregion
     }
