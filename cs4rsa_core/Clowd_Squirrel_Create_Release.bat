ca1a0517a61165158fbe004e040a8c9cb4fae537
axin <truongaxin123@gmail.com>
2023-03-12 22:24:36 +0700

    [CS0061] Update Batch file

--- cs4rsa_core/Clowd_Squirrel_Create_Release.bat
+++ cs4rsa_core/Clowd_Squirrel_Create_Release.bat
@@ -1,35 +1,64 @@
 echo off
+cls
 @REM Run this script in the CMD when release
 @REM https://github.com/clowd/Clowd.Squirrel
 @REM Create Release
 
-echo ======================================================================
-echo = Crediz Deploy script                                               =
-echo = Create a Squirrel release using the Squirrel.exe command line tool =
-echo ======================================================================
+call :PrintGreenText ===============================================================================
+call :PrintGreenText =                                 Crediz Deploy script                        =
+call :PrintGreenText =          Create a Squirrel release using the Squirrel.exe command line tool =
+call :PrintGreenText ===============================================================================
 
-echo ======================================================================
-echo = 1. Run MSBuild And donet publish                                   =
-echo ======================================================================
-call msbuild
-call dotnet publish
-
-set packId=CredizPackId
-
+@REM Application Name show in Icon, Control Panel
+set packId=Crediz
 @REM Change packVersion when change version of application to apply update
-set packVersion=1.0.1-pre
+set packVersion=1.0.2
+set packAuthor="CS4RSA by Truong A Xin"
+set framework=net6,vcredist143-x86
+set pathSquirrel=%userprofile%\.nuget\packages\clowd.squirrel\2.9.42\tools\Squirrel.exe
+set pathIconForSetupAndUpdate=AppResources\logo.ico
+set pathSplashImage=AppResources\setupSplashScreen.gif
+set pathReleaseDir=Releases\
+set packDirectory=bin\Release\net6.0-windows\publish
 
-set packDirectory=C:\Users\truon\source\repos\cs4rsa_core\cs4rsa_core\bin\Debug\net6.0-windows\publish
-set framework=net6.0-x64
+call :PrintYellowText ======================================================================
+call :PrintYellowText =                1. Run dotnet build And donet publish               =
+call :PrintYellowText ======================================================================
+set dotnetBuildAndPublishCommand= dotnet publish ^
+        --configuration Release ^
+        --no-dependencies ^
+        --output %packDirectory%
+call dotnet msbuild
+call %dotnetBuildAndPublishCommand%
 
-echo Run Squirrel.exe
-set SquirrelReleaseCommand=C:\Users\truon\.nuget\packages\clowd.squirrel\2.9.42\tools\Squirrel.exe pack --packId=%packId% --packVersion=%packVersion% --packDirectory=%packDirectory% --framework=%framework%
+call :PrintYellowText ======================================================================
+call :PrintYellowText =                        2. Run Squirrel pack                        =
+call :PrintYellowText ======================================================================
+set SquirrelReleaseCommand=%pathSquirrel% pack ^
+    --packId=%packId% ^
+    --packVersion=%packVersion% ^
+    --packDirectory=%packDirectory% ^
+    --packAuthors=%packAuthor% ^
+    --framework=%framework% ^
+    --icon=%pathIconForSetupAndUpdate% ^
+    --splashImage=%pathSplashImage% ^
+    --releaseDir=%pathReleaseDir%
 call %SquirrelReleaseCommand%
 
-echo =============================
-echo = Create Release Successed! =
-echo =============================
+call :PrintGreenText ================================
+call :PrintGreenText =  Create Release Successed!   =
+call :PrintGreenText ================================
 
 echo Let push all file in the followed folder to Github release
-echo C:\Users\truon\source\repos\cs4rsa_core\cs4rsa_core\Releases\
-call explorer C:\Users\truon\source\repos\cs4rsa_core\cs4rsa_core\Releases\
+echo Check out the %pathReleaseDir% folder
+call explorer %pathReleaseDir%
+
+@REM Print to console a green text
+:PrintGreenText
+echo [92m%*[0m
+exit /B 0
+
+@REM Print to console a yellow text
+:PrintYellowText
+echo [93m%*[0m
+exit /B 0
